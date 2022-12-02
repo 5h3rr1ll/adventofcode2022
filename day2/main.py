ae405301e58e08c9520607eb6a7a00d3ea61d426
anthony <a@sherrill.de>
2022-12-02 15:55:44 +0100

    day2 task 2 solved

--- day2/main.py
+++ day2/main.py
@@ -3,16 +3,13 @@
 
 
 objects = {
-    "A":{"name":"rock", "value": 1},
-    "B":{"name":"paper", "value": 2},
-    "C":{"name":"scissors", "value": 3},
-    "X":{"name":"rock", "value": 1},
-    "Y":{"name":"paper", "value": 2},
-    "Z":{"name":"scissors", "value": 3}
+    "A":{"name":"rock", "value": 1, "X": "scissors", "Z": "paper", "Y": "rock"},
+    "B":{"name":"paper", "value": 2, "X": "rock", "Z": "scissors", "Y": "paper"},
+    "C":{"name":"scissors", "value": 3, "X": "paper", "Z": "rock", "Y": "scissors"},
+    "X":{"name":"rock", "value": 1, "X": "scissors", "Z": "paper", "Y": "rock"},
+    "Y":{"name":"paper", "value": 2, "X": "rock", "Z": "scissors", "Y": "paper"},
+    "Z":{"name":"scissors", "value": 3, "X": "paper", "Z": "rock", "Y": "scissors"}
 } 
-
-
-score = 0
 
 def win_logic(a, b):
     object_value_a, object_value_b = objects[a]["value"], objects[b]["value"]
@@ -35,15 +32,42 @@ def object_logic(objec_name):
             return 2
         case "scissors":
             return 3
+        
+def win_lose_pat(xyz):
+    match xyz:
+        case "X":
+            return 0
+        case "Y":
+            return 3
+        case "Z":
+            return 6
 
+def choose_the_right_object(object, instruction):
+    return object_logic(objects[object][instruction])
 
-with open("f.txt", "r") as f:
-    for line in f:
-        a,b = line.split(" ")
-        a,b = a.replace("\n", ""), b.replace("\n", "")
-        print(objects[a]["name"], objects[b]["name"])
-        win = win_logic(a,b)
-        obj_value = object_logic(objects[b]["name"])
-        score += win + obj_value
-        # userinput = input(f"Your score is {score}. Press enter to continue")
-    print(score)
+def part1():
+    score = 0
+    with open("f.txt", "r") as f:
+        for line in f:
+            a,b = line.split(" ")
+            a,b = a.replace("\n", ""), b.replace("\n", "")
+            win = win_logic(a,b)
+            obj_value = object_logic(objects[b]["name"])
+            score += win + obj_value
+            # userinput = input(f"Your score is {score}. Press enter to continue")
+        print(score)
+
+def part2():
+    score = 0
+    with open("f.txt", "r") as f:
+        for line in f:
+            a,b = line.split(" ")
+            a,b = a.replace("\n", ""), b.replace("\n", "")
+            instructions = win_lose_pat(b)
+            obj_value = choose_the_right_object(a, b)
+            score += instructions + obj_value
+            # userinput = input(f"Your score is {score}. Press enter to continue")
+        print(score)
+
+part1()
+part2()
